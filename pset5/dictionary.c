e948e2f217dd8b97e1b99094118580ed7fa06892
RegretGG <69192923+RegretGG@users.noreply.github.com>
2021-05-21 20:19:23 +0400

    Add files via upload

---
+++ pset5/dictionary.c
@@ -0,0 +1,114 @@
+// Implements a dictionary's functionality
+
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+int word_count = 0;
+
+
+
+#include "dictionary.h"
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+}
+node;
+
+// Number of buckets in hash table
+const unsigned int N = 676;
+
+// Hash table
+node *table[N];
+
+// Returns true if word is in dictionary, else false
+bool check(const char *word)
+{
+    // TODO
+    int hash_value = hash(word);
+    node *n = table[hash_value];
+    while(n != NULL)
+    {
+        if (strcasecmp(word, n->word) == 0)
+        {
+            return true;
+        }
+        n = n->next;
+    }
+    return false;
+}
+
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+    // TODO
+long sum = 0;
+    for (int i = 0; i < strlen(word); i++)
+    {
+        sum += tolower(word[i]);
+    }
+    return sum % N;
+}
+// Loads dictionary into memory, returning true if successful, else false
+bool load(const char *dictionary)
+{
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        return false;
+    }
+    char word[LENGTH + 1];
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return false;
+        }
+        strcpy(n->word, word);
+        int hash_value = hash(word);
+        n->next = table[hash_value];
+        table[hash_value] = n;
+        word_count++;
+    }
+    fclose(file);
+    return true;
+    }
+
+// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+unsigned int size(void)
+{
+    if (word_count > 0)
+    {
+        return word_count;
+    }
+
+    return 0;
+}
+
+// Unloads dictionary from memory, returning true if successful, else false
+bool unload(void)
+{
+    // TODO
+    for (int i = 0; i < N; i++)
+    {
+        node *n = table[i];
+        while(n != NULL)
+        {
+            node *temp = n;
+            n = n->next;
+            free(temp);
+        }
+        if (i == N-1 && n == NULL)
+        {
+            return true;
+        }
+    }
+    
+    return false;
+}
