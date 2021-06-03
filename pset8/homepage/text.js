ef4b60b906ef625eee2ec27ea5cc4eb41356e2e0
RegretGG <69192923+RegretGG@users.noreply.github.com>
2021-06-03 23:06:20 +0400

    Add files via upload

---
+++ pset8/homepage/text.js
@@ -0,0 +1,7 @@
+var button = document.getElementById("jser");
+var h3 = document.getElementById("js");
+button.onclick = function(){
+    document.getElementById('cat').style.cssText = 'visibility: visible;';
+    h3.innerHTML = "There, much better now don't you think?";
+
+};
